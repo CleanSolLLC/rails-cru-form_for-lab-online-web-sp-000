dea67c07f80574550dce4efdc2361a43f6736f71
Mark May <mamark90@gmail.com>
2020-10-06 23:27:42 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -22,7 +22,7 @@ class GenresController < ApplicationController
   end
 
   private
-    def artist_params(*args)
-      params.require(:artist).permit(*args)
+    def genre_params(*args)
+      params.require(:genre).permit(*args)
     end
 end
