89804c29fe771561de82a7cd23b84a51a177ee0c
Mark May <mamark90@gmail.com>
2020-10-06 23:27:23 +0000

    Automatically backed up by Learn

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,28 @@
 class GenresController < ApplicationController
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(genre_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.update(artist_params(:name))
+    redirect_to genre_path(@genre)
+  end
+
+  private
+    def artist_params(*args)
+      params.require(:artist).permit(*args)
+    end
 end
